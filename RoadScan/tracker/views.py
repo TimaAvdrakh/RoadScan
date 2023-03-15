dfd25e3995d9dac625320f8e1c9dc351442c92e6
tima <t.abdrakhmanov@esterna.eu>
2023-03-15 19:17:21 +0600

    add : requirements file

--- RoadScan/tracker/views.py
+++ RoadScan/tracker/views.py
@@ -12,5 +12,13 @@ class ShopsListCreateAPIView(ListCreateAPIView):
     def get_queryset(self):
         return Shop.objects.all()
 
-
+    # def create(self, request, *args, **kwargs):
+"""
+    get all cracks which are close to 1 meter
+    if there are approveds make all approved and join
+    else
+    add increese counter
+        and if counter > 3  make crack approved
+"""
+    # return
 list_create_shops = ShopsListCreateAPIView.as_view()
