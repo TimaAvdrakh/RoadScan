47fb5f90f059484c676fef7a4df2b738662da037
Abdr <102943880+Asan1900@users.noreply.github.com>
2023-03-15 21:14:55 +0600

    Add @admin.register, give two different verification and model, view

--- RoadScan/tracker/admin.py
+++ RoadScan/tracker/admin.py
@@ -1,7 +1,12 @@
 from django.contrib.gis.admin import OSMGeoAdmin, GeoModelAdmin
-from .models import Shop
+from .models import RoadCrack
+from .models import Police
 from django.contrib import admin
 
-@admin.register(Shop)
-class ShopAdmin(GeoModelAdmin):
-    list_display = ('name', 'location')
+@admin.register(RoadCrack)
+class Admin(GeoModelAdmin):
+    list_display = ('name', 'location', 'counter', 'approve', 'created_at')
+
+@admin.register(Police)
+class Admin(OSMGeoAdmin):
+    list_display = ("name", "address", "city", "approve", "created_at")
