db19858c59669c561bd40933f6fab37e6775ea52
Abdr <102943880+Asan1900@users.noreply.github.com>
2023-03-16 09:43:22 +0600

    Add class RoadClass - new Model: counter, address, city Add a approve function (if >= 5) Add class Police - new Model: Appove,, created_at

--- RoadScan/tracker/models.py
+++ RoadScan/tracker/models.py
@@ -1,9 +1,38 @@
 from django.db import models
 from django.contrib.gis.db import models
-# Create your models here.
 
-class Shop(models.Model):
-    name = models.CharField(max_length=100)
+
+class RoadCrack(models.Model):
+    name = models.CharField(max_length=255)
     location = models.PointField()
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
+    counter = models.IntegerField()
+    approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    RoadCrack = RoadCrack.objects.get(id=0)
+    RoadCrack.counter += 1
+    RoadCrack.approve()
+    RoadCrack.save()
+
+    def approve(self):
+        if self.counter >= 5:
+            self.counter = True
+        else:
+            self.counter = False
+
+
+class Police(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.PointField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    approve = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Police in {self.city}"
