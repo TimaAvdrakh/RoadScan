898f52a8a5b68f296624b27148b78d16faf72856
Abdr <102943880+Asan1900@users.noreply.github.com>
2023-03-16 09:47:24 +0600

    import crack serializer, render, road-crack and police add request - road_crack_list and police_list

--- RoadScan/tracker/views.py
+++ RoadScan/tracker/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 
-from .models import Shop
-from .serializers import ShopSerializer
+from .serializers import CrackSerializer
+from django.shortcuts import render
+from .models import RoadCrack, Police
 
 
-# Create your views here.
-class ShopsListCreateAPIView(ListCreateAPIView):
-    serializer_class = ShopSerializer
+class CrackListCreateAPIView(ListCreateAPIView):
+    serializer_class = CrackSerializer
 
-    def get_queryset(self):
-        return Shop.objects.all()
+    def road_crack_list(request):
+        road_cracks = RoadCrack.objects.all()
+        return render(request, {'road_cracks': road_cracks})
+
+    def police_list(request):
+        police = Police.objects.all()
+        return render(request, {'police': police})
 
 
-list_create_shops = ShopsListCreateAPIView.as_view()
+list_create_crack = CrackListCreateAPIView.as_view()
