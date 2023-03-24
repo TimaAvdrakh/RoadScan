ea8443bc771021c5247aebb1f4d237a033a2cf15
bivashy <botyrbojey@gmail.com>
2023-03-24 21:38:06 +0600

    Remove redundant max_length attribute on IntegerField

--- tracker/models.py
+++ tracker/models.py
@@ -14,7 +14,7 @@ class RoadCrack(models.Model):
     city = models.CharField(max_length=50, default="Almaty", editable=False)
     danger_level = models.CharField(max_length=10, choices=DangerLevel.choices, default=DangerLevel.LOW.value)
     approved = models.BooleanField(default=False)
-    requested_amount = models.IntegerField(max_length=10, default=1)
+    requested_amount = models.IntegerField(default=1)
 
 
 class PoliceBump(models.Model):
