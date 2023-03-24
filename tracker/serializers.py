47b9343c791dd68e820b468d87994e015d35a886
bivashy <botyrbojey@gmail.com>
2023-03-24 20:34:12 +0600

    Remove redundant code related with "Shop" model

--- tracker/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Shop
-from rest_framework import serializers
-
-class ShopSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Shop
-        fields = ("name", "location", "address", "city")
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,4 @@
-from django.urls import path
-from .views import list_create_shops
-
 app_name = 'tracker'
 
 urlpatterns = [
-    path('', list_create_shops, name="list")
-    # path('<int:pk>/', TodoDetailAPIView.as_view(), name="detail"),
 ]
