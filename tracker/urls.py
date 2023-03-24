a3915fee2f41572856660b4d63ff293250abaa00
bivashy <botyrbojey@gmail.com>
2023-03-24 21:39:36 +0600

    Add endpoints for RoadCrack, PoliceBump views

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,4 +1,10 @@
+from django.urls import path
+
+from tracker import views
+
 app_name = 'tracker'
 
 urlpatterns = [
+    path('', views.RoadCrackListAPIView.as_view()),
+    path('policebump', views.PoliceBumpListAPIView.as_view())
 ]
