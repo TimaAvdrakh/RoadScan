3229d6332e37287acd4962682d197b8348c8695a
tima <t.abdrakhmanov@esterna.eu>
2023-03-29 18:53:37 +0600

    add : model lat, long fields

--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,5 @@
 from django.contrib.gis.db import models
-
+from django.contrib.gis.geos import Point
 
 class DangerLevel(models.TextChoices):
     LOW = 'l', "Low"
@@ -9,15 +9,27 @@ class DangerLevel(models.TextChoices):
 
 class RoadCrack(models.Model):
     id_station = models.AutoField(primary_key=True)
-    location = models.PointField()
+    latitude = models.FloatField(blank=True, null=True, verbose_name='Latitude')
+    longitude = models.FloatField(blank=True, null=True, verbose_name='Longitude')
+    location = models.PointField(blank=True, null=True, srid=4326)
     address = models.CharField(max_length=100, default="unknown")
     city = models.CharField(max_length=50, default="Almaty", editable=False)
-    danger_level = models.CharField(max_length=10, choices=DangerLevel.choices, default=DangerLevel.LOW.value)
+    danger_level = models.CharField(max_length=10, choices=DangerLevel.choices, default=DangerLevel.LOW)
     approved = models.BooleanField(default=False)
     requested_amount = models.IntegerField(default=1)
 
+    def save(self, *args, **kwargs):
+        self.location = Point(self.longitude, self.latitude)
+        super(RoadCrack, self).save(*args, **kwargs)  # Call the "real" save() method.
+
 
 class PoliceBump(models.Model):
-    location = models.PointField()
+    latitude = models.FloatField(blank=True, null=True, verbose_name='Latitude')
+    longitude = models.FloatField(blank=True, null=True, verbose_name='Longitude')
+    location = models.PointField(blank=True, null=True, srid=4326)
     address = models.CharField(max_length=100, default="unknown")
     city = models.CharField(max_length=50, default="Almaty", editable=False)
+
+    def save(self, *args, **kwargs):
+        self.location = Point(self.longitude, self.latitude)
+        super(PoliceBump, self).save(*args, **kwargs)
