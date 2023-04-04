93e21a232557c44f14e4ba1b378d6bd997c3b348
tima <t.abdrakhmanov@esterna.eu>
2023-04-05 00:17:15 +0600

    gdal configs

--- RoadScan/settings.py
+++ RoadScan/settings.py
@@ -134,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
